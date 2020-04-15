03c525bf17f10b42a5f1f0f815d0562eb70bd2ee
Marc Cornette <marccornette@hotmail.fr>
2020-04-15 14:43:43 +0200

    Update french fr.js

--- locale/fr.js
+++ locale/fr.js
@@ -1,56 +1,51 @@
 /***********************************************************************************************************************
-
 	fr.js – Français
-
 	Localization by: Pierre Kessler.
-
 	Copyright © 2017 Thomas Michael Edwards <thomasmedwards@gmail.com>. All rights reserved.
 	Use of this source code is governed by a BSD 2-clause "Simplified" License, which may be found in the LICENSE file.
-
 	For more information about the guidelines used to create this localization, see:
 		http://www.motoslave.net/sugarcube/2/docs/#guide-localization
-
 ***********************************************************************************************************************/
 (function () {
 	/* General. */
 	l10nStrings.identity = 'aventure';
-	l10nStrings.aborting = 'Echec';
+	l10nStrings.aborting = 'Abandon';
 	l10nStrings.cancel   = 'Annuler';
 	l10nStrings.close    = 'Fermer';
 	l10nStrings.ok       = 'OK';
 
 	/* Errors. */
 	l10nStrings.errorTitle              = 'Erreur';
-	l10nStrings.errorNonexistentPassage = 'le passage "{passage}" n\'existe pas.';
-	l10nStrings.errorSaveMissingData    = 'des éléments manquent dans la sauvegarde. Soit ce fichier n\'est pas une sauvegarde; soit il est corrumpu';
-	l10nStrings.errorSaveIdMismatch     = 'la sauvegarde provient d\'une fausse {identity}';
+	l10nStrings.errorNonexistentPassage = 'le passage "{passage}" n\'existe pas';
+	l10nStrings.errorSaveMissingData    = 'des éléments manquent dans cette sauvegarde. Soit ce fichier n\'en est pas une, soit il est corrompu';
+	l10nStrings.errorSaveIdMismatch     = 'la sauvegarde ne semble pas provenir de cette {identity}';
 
 	/* Warnings. */
-	l10nStrings._warningIntroLacking  = 'Votre navigateur est incapable ou n\'autorise pas de ';
-	l10nStrings._warningOutroDegraded = '; ainsi {identity} fonctionne en mode dégradé. Vous pourrez peut-être continuer; mais certaines séquences risquent de ne pas fonctionner.';
+	l10nStrings._warningIntroLacking  = 'Soit il manque à votre navigateur, soit il n\'autorise pas';
+	l10nStrings._warningOutroDegraded = ', de ce fait l\'{identity} sera en mode dégradé. Cela n\'empechera le fonctionnement, mais vous risquez de rencontrer des erreurs.';
 	l10nStrings.warningNoWebStorage   = '{_warningIntroLacking} l\'API de stockage de données{_warningOutroDegraded}';
-	l10nStrings.warningDegraded       = '{_warningIntroLacking} certaines des fonctionnalités requises par {identity}{_warningOutroDegraded}';
+	l10nStrings.warningDegraded       = '{_warningIntroLacking} certaines des fonctionnalités requises par l\'{identity}{_warningOutroDegraded}';
 
 	/* Debug View. */
 	l10nStrings.debugViewTitle  = 'Mode débogage';
-	l10nStrings.debugViewToggle = 'Basculer le mode débogage';
+	l10nStrings.debugViewToggle = 'Basculer en mode débogage';
 
 	/* UI bar. */
-	l10nStrings.uiBarToggle   = 'Affiche/cache la barre d\'interface';
-	l10nStrings.uiBarBackward = 'Aller en arrière dans l\'aventure {identity}';
-	l10nStrings.uiBarForward  = 'Aller en arrière dans l\'aventure {identity}';
-	l10nStrings.uiBarJumpto   = 'Aller à un point précis de l\'aventure {identity}';
+	l10nStrings.uiBarToggle   = 'Afficher/cacher la barre d\'interface';
+	l10nStrings.uiBarBackward = 'Revenir en arrière dans l\'histoire de l\'{identity}';
+	l10nStrings.uiBarForward  = 'Avancer en arrière dans l\'histoire de l\'{identity}';
+	l10nStrings.uiBarJumpto   = 'Aller à un point précis de l\'histoire de l\'{identity}';
 
 	/* Jump To. */
-	l10nStrings.jumptoTitle       = 'Sauter à';
-	l10nStrings.jumptoTurn        = 'Tourner';
-	l10nStrings.jumptoUnavailable = 'Aucun point de saut disponible maintenant\u2026';
+	l10nStrings.jumptoTitle       = 'Aller à'; //'au' if {passage} is next
+	l10nStrings.jumptoTurn        = 'Bifurquer';
+	l10nStrings.jumptoUnavailable = 'Aucun point d\'arriver n\'est disponible\u2026';
 
 	/* Saves. */
 	l10nStrings.savesTitle       = 'Sauvegardes';
 	l10nStrings.savesDisallowed  = 'Les sauvegardes sont désactivées pour ce passage.';
 	l10nStrings.savesEmptySlot   = '\u2014 emplacement vide \u2014';
-	l10nStrings.savesIncapable   = '{_warningIntroLacking} n\'a pas la capacité requise : les sauvegardes sont désactivées pour cette session.';
+	l10nStrings.savesIncapable   = '{_warningIntroLacking} soit il n\'a pas la capacité pour prendre en charge les sauvegardes : elles seront désactivées pour cette session.';
 	l10nStrings.savesLabelAuto   = 'Sauvegarde Auto';
 	l10nStrings.savesLabelDelete = 'Effacer';
 	l10nStrings.savesLabelExport = 'Enregistrer sur le disque\u2026';
@@ -60,7 +55,7 @@
 	l10nStrings.savesLabelSave   = 'Enregistrer';
 	l10nStrings.savesLabelSlot   = 'Emplacement';
 	l10nStrings.savesSavedOn     = 'Enregistré sur';
-	l10nStrings.savesUnavailable = 'Aucun emplacement de sauvegarde trouvé\u2026';
+	l10nStrings.savesUnavailable = 'Aucun emplacement de sauvegarde n\'a été trouvé\u2026';
 	l10nStrings.savesUnknownDate = 'inconnu';
 
 	/* Settings. */
@@ -71,7 +66,7 @@
 
 	/* Restart. */
 	l10nStrings.restartTitle  = 'Recommencer';
-	l10nStrings.restartPrompt = 'Êtes vous sûr(e) de vouloir recommencer ? Cette partie sera perdue.';
+	l10nStrings.restartPrompt = 'Êtes vous sûr(e) de vouloir recommencer ? Tous progrès non sauvegardés seront perdus.';
 
 	/* Share. */
 	l10nStrings.shareTitle = 'Partager';
