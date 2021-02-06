e281651f9f5f74bb1997705e6dca190dced8b45b
Thomas M. Edwards <thomasmedwards@gmail.com>
2021-02-05 23:13:37 -0600

    UPDATE: Disallow the core document elements in `stringFrom()`.

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -389,6 +389,14 @@ var { // eslint-disable-line no-var
 				return value.toLocaleString();
 			}
 			else if (value instanceof Element) {
+				if (
+					value === document.documentElement
+					|| value === document.head
+					|| value === document.body
+				) {
+					throw new Error('illegal operation; attempting to convert the <html>, <head>, or <body> tags to string is not allowed');
+				}
+
 				return value.outerHTML;
 			}
 			else if (value instanceof Node) {
