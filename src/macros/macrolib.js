11838cab535b166d40abb4235f495b012accf224
Thomas M. Edwards <thomasmedwards@gmail.com>
2021-03-20 15:17:41 -0500

    UPDATE: Allow non-void widgets and new widget variables.
    * Allow non-void widgets via `block` keyword.
    * Add widget variables `_args` and `_contents`.
    * Deprecate widget variable `$args`.

--- src/macros/macrolib.js
+++ src/macros/macrolib.js
@@ -3702,6 +3702,7 @@
 			}
 
 			const widgetName = this.args[0];
+			const isNonVoid  = this.args.length > 1 && this.args[1] === 'block';
 
 			if (Macro.has(widgetName)) {
 				if (!Macro.get(widgetName).isWidget) {
@@ -3713,30 +3714,52 @@
 			}
 
 			try {
-				Macro.add(widgetName, {
+				const widgetDef = {
 					isWidget : true,
-					handler  : (function (contents) {
+					handler  : (function (widgetCode) {
 						return function () {
-							let argsCache;
+							const shadowStore = {};
 
+							// Cache the existing value of the `_args` variable, if necessary.
+							if (State.temporary.hasOwnProperty('args')) {
+								shadowStore._args = State.temporary.args;
+							}
+
+							// Set up the widget `_args` variable and add a shadow.
+							State.temporary.args = [...this.args];
+							State.temporary.args.raw = this.args.raw;
+							State.temporary.args.full = this.args.full;
+							this.addShadow('_args');
+
+							if (isNonVoid) {
+								// Cache the existing value of the `_contents` variable, if necessary.
+								if (State.temporary.hasOwnProperty('contents')) {
+									shadowStore._contents = State.temporary.contents;
+								}
+
+								// Set up the widget `_contents` variable and add a shadow.
+								State.temporary.contents = this.payload[0].contents;
+								this.addShadow('_contents');
+							}
+
+							/* legacy */
 							// Cache the existing value of the `$args` variable, if necessary.
 							if (State.variables.hasOwnProperty('args')) {
-								argsCache = State.variables.args;
+								shadowStore.$args = State.variables.args;
 							}
 
 							// Set up the widget `$args` variable and add a shadow.
-							State.variables.args = [...this.args];
-							State.variables.args.raw = this.args.raw;
-							State.variables.args.full = this.args.full;
+							State.variables.args = State.temporary.args;
 							this.addShadow('$args');
+							/* /legacy */
 
 							try {
 								// Set up the error trapping variables.
 								const resFrag = document.createDocumentFragment();
 								const errList = [];
 
-								// Wikify the widget contents.
-								new Wikifier(resFrag, contents);
+								// Wikify the widget's code.
+								new Wikifier(resFrag, widgetCode);
 
 								// Carry over the output, unless there were errors.
 								Array.from(resFrag.querySelectorAll('.error')).forEach(errEl => {
@@ -3747,24 +3770,50 @@
 									this.output.appendChild(resFrag);
 								}
 								else {
-									return this.error(`error${errList.length > 1 ? 's' : ''} within widget contents (${errList.join('; ')})`);
+									return this.error(`error${errList.length > 1 ? 's' : ''} within widget code (${errList.join('; ')})`);
 								}
 							}
 							catch (ex) {
 								return this.error(`cannot execute widget: ${ex.message}`);
 							}
 							finally {
+								// Revert the `_args` variable shadowing.
+								if (shadowStore.hasOwnProperty('_args')) {
+									State.temporary.args = shadowStore._args;
+								}
+								else {
+									delete State.temporary.args;
+								}
+
+								if (isNonVoid) {
+									// Revert the `_contents` variable shadowing.
+									if (shadowStore.hasOwnProperty('_contents')) {
+										State.temporary.contents = shadowStore._contents;
+									}
+									else {
+										delete State.temporary.contents;
+									}
+								}
+
+								/* legacy */
 								// Revert the `$args` variable shadowing.
-								if (typeof argsCache !== 'undefined') {
-									State.variables.args = argsCache;
+								if (shadowStore.hasOwnProperty('$args')) {
+									State.variables.args = shadowStore.$args;
 								}
 								else {
 									delete State.variables.args;
 								}
+								/* /legacy */
 							}
 						};
 					})(this.payload[0].contents)
-				});
+				};
+
+				if (isNonVoid) {
+					widgetDef.tags = [];
+				}
+
+				Macro.add(widgetName, widgetDef);
 
 				// Custom debug view setup.
 				if (Config.debug) {
