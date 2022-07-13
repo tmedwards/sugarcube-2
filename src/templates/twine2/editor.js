0368587f09d22b2f01d6dac38211764e9fd21126
Thomas M. Edwards <thomasmedwards@gmail.com>
2022-07-13 16:31:54 -0500

    FEAT: Add Twine 2 editor extensions file
    Just creating the file and the branch for now.

---
+++ src/templates/twine2/editor.js
@@ -0,0 +1,10 @@
+/***********************************************************************************************************************
+
+	editor.js
+
+	Copyright © 2022 Thomas Michael Edwards <thomasmedwards@gmail.com>. All rights reserved.
+	Use of this source code is governed by a BSD 2-clause "Simplified" License, which may be found in the LICENSE file.
+
+***********************************************************************************************************************/
+
+/* Twine 2 editor extensions go here. */
