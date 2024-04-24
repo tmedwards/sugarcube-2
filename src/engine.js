9dc6ddfe1a1ce2b5668b3dcaaf442923eed03593
Thomas M. Edwards <thomasmedwards@gmail.com>
2024-04-24 08:58:35 -0500

    refactor: enforce native `#story` primacy

--- src/engine.js
+++ src/engine.js
@@ -60,7 +60,7 @@ var Engine = (() => { // eslint-disable-line no-unused-vars, no-var
 		// Get the user's UI markup, if any.
 		const markup = Story.has('StoryInterface') && Story.get('StoryInterface').text.trim();
 
-		// If user markup exists, remove unnecessay default UI and process the markup.
+		// If user markup exists, remove unnecessary default UI and process the markup.
 		if (markup) {
 			// Disable updates on default UI elements.
 			Config.ui.updateStoryElements = false;
@@ -73,6 +73,11 @@ var Engine = (() => { // eslint-disable-line no-unused-vars, no-var
 
 			// Append the user's UI elements to the main UI container.
 			$main.append(markup);
+
+			// Bail out if a child `#story` exists.
+			if ($main.find('#story').length > 0) {
+				throw new Error('element with ID "story" found within "StoryInterface" special passage');
+			}
 
 			// Cache the `#passages` element.
 			const $passages = $main.find('#passages');
