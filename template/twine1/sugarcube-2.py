68c6623e413d4213aac5ec5c069d3006f2c1f927
Thomas M. Edwards <thomasmedwards@gmail.com>
2025-08-13 16:26:47 -0500

    Update sugarcube-2.py

--- template/twine1/sugarcube-2.py
+++ template/twine1/sugarcube-2.py
@@ -2,7 +2,7 @@
 #
 # sugarcube-2.py
 #
-# Copyright (c) 2013-2021 Thomas Michael Edwards <thomasmedwards@gmail.com>. All rights reserved.
+# Copyright (c) 2013-2025 Thomas Michael Edwards <thomasmedwards@gmail.com>. All rights reserved.
 # Use of this source code is governed by a BSD 2-clause "Simplified" License, which may be found in the LICENSE file.
 #
 ########################################################################################################################
